d48ccebe92e54255be0d4984bd2083f0f8fbc0c7
GENRazen <mhmmdr4f1a@gmail.com>
2024-09-30 10:44:12 +0700

    30-09

--- app/Http/Controllers/CandidateCont.php
+++ app/Http/Controllers/CandidateCont.php
@@ -13,15 +13,28 @@ class CandidateCont extends Controller
         $jobs = job::get();
         return view('candidate.candidate', ['candidates' => $candidates,'jobs' => $jobs]);
     }
-
+    
     public function delete(Request $req){
         $ids = $req->input('ids');
-
+        
         if (!empty($ids)) {
             $delete = candidate::whereIn('id_candidate', $ids)->delete();
             return response()->json(['success' => true]);
         } else {
             return response()->json(['success' => false]);
         }
+    }
+
+    public function candidateDetail(Request $req){
+        $candidates = candidate::get();
+        $jobs = job::get();
+        return view('candidate.detail_candidates', ['candidates' => $candidates,'jobs' => $jobs]);
+    }
+
+    public function candidateDetailView(){
+        $candidates = candidate::get();
+        $first = candidate::first();
+        $jobs = job::get();
+        return view('candidate.detail_candidates', ['candidates' => $candidates,'jobs' => $jobs,'first' => $first]);
     }
 }
